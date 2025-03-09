a39989317fc37eeac1ed9cda572fb560a05c280c
Titu Ray choudhury <tituraychoudhury@gmail.com>
2025-03-09 16:19:10 +0530

    define auth routes

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/tr-choudhury21/prepportal_backend/config"
+	"github.com/tr-choudhury21/prepportal_backend/routes"
 )
 
 func main() {
@@ -19,26 +19,21 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	//connect db
 	client := config.ConnectDB()
 	defer client.Disconnect(context.Background())
-
 	fmt.Println("MongoDB client connected and ready to use!")
 
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to NITA PrepPortal API",
-		})
-	})
+	//routes
+	routes.AuthRoutes(router)
 
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		port = "8080"
 	}
 
 	fmt.Println("🚀 Server running on Port:", port)
-
 	router.Run(":" + port)
 }
